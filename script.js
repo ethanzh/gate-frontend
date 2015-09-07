de22ac64865237956209f756ccf63ea253138c4d
Ethan Houston <ethan.houston@gmail.com>
2015-09-07 11:15:02 +0800

    Fixed mouseover events

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-//"use strict"
+"use strict"
 var loginGrabber = function(){
 	var username = document.getElementById("username").value
 	var password = document.getElementById("password").value
 }
 
+$(document).ready(function(){
+	$( "#usr" ).mouseover(function() {
+	  $("#usr").css("background-color", "#66B2FF");
+	});
+	$( "#usr" ).mouseleave(function() {
+	  $("#usr").css("background-color", "white");
+	});
+	$( "#pas" ).mouseover(function() {
+	  $("#pas").css("background-color", "#66B2FF");
+	});
+	$( "#pas" ).mouseleave(function() {
+	  $("#pas").css("background-color", "white");
+	});
+});
 
-$( "#usr" ).mouseover(function() {
-  $("#usr").css("background-color", "#66B2FF");
-});
-$( "#usr" ).mouseleave(function() {
-  $("#usr").css("background-color", "white");
-});
-$( "#pas" ).mouseover(function() {
-  $("#pas").css("background-color", "#66B2FF");
-});
-$( "#pas" ).mouseleave(function() {
-  $("#pas").css("background-color", "white");
-});
