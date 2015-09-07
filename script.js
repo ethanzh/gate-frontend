67c7e3169df5bbe5b7aa4caedf022d904a9c2ce8
Ethan Houston <ethan.houston@gmail.com>
2015-09-07 11:05:22 +0800

    Added mouseon events

---
+++ script.js
@@ -0,0 +1,19 @@
+//"use strict"
+var loginGrabber = function(){
+	var username = document.getElementById("username").value
+	var password = document.getElementById("password").value
+}
+
+
+$( "#usr" ).mouseover(function() {
+  $("#usr").css("background-color", "#66B2FF");
+});
+$( "#usr" ).mouseleave(function() {
+  $("#usr").css("background-color", "white");
+});
+$( "#pas" ).mouseover(function() {
+  $("#pas").css("background-color", "#66B2FF");
+});
+$( "#pas" ).mouseleave(function() {
+  $("#pas").css("background-color", "white");
+});
