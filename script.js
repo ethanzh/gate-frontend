ff990a652ea8eebcf4c0bd3aedd9fd235027065a
Ethan Houston <ethan.houston@gmail.com>
2015-09-08 14:48:09 +0800

    added isEmail function

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 var loginGrabber = function(){
 	var username = document.getElementById("username").value
 	var password = document.getElementById("password").value
-	username = username.trim().toLowerCase();
-	password = password.trim().toLowerCase();
+	username = username.toLowerCase();
+	username = username.trim();
 	console.log(username + " " + password);
+	console.log(isEmail(username));
+}
+var isEmail = function(input){
+	if (input.indexOf("@") > 0){
+		return true
+	}
+	else{
+		return false
+	}
 }
 $(document).ready(function(){
 	$( "#usr" ).mouseover(function() {
